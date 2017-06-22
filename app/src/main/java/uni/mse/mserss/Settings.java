8b060dd436f377bbce32d3683f42d6620eee602b
marcel <mpnw47@gmail.com>
2017-06-22 20:53:40 +0200

    Filled Settings Activity

--- app/src/main/java/uni/mse/mserss/Settings.java
+++ app/src/main/java/uni/mse/mserss/Settings.java
@@ -1,6 +1,7 @@
 package uni.mse.mserss;
 
 import android.app.AlarmManager;
+import android.content.Context;
 
 /**
  * Created by marcel on 17.05.17.
@@ -11,7 +12,6 @@ public abstract class Settings {
     public static int Interval = 15;
 
     public static void setInterval(int interval) {
-
     }
 
     public static void getInterval() {
--- app/src/main/java/uni/mse/mserss/SettingsActivity.java
+++ app/src/main/java/uni/mse/mserss/SettingsActivity.java
@@ -1,16 +1,24 @@
 package uni.mse.mserss;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends Activity {
 
     private Button btSettingsApply;
     private Spinner spinnerSettings;
+    private ArrayList<String> interval;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,37 @@ public class SettingsActivity extends Activity {
 
         btSettingsApply = (Button) findViewById(R.id.btSettingsApply);
         spinnerSettings = (Spinner) findViewById(R.id.spinnerSettings);
+        interval = new ArrayList();
+
+        interval.add("15 minutes");
+        interval.add("30 minutes");
+        interval.add("60 minutes");
+        interval.add("120 minutes");
+
+        spinnerSettings.setAdapter(new ArrayAdapter<>(this, R.layout.spinner_item, interval));
+        spinnerSettings.setSelection(0);
+
+        LoadSettings();
 
         btSettingsApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Settings.setInterval(spinnerSettings.getSelectedItemPosition());
+                SaveSettings();
+                finish();
             }
         });
+    }
+
+    private void LoadSettings() {
+        preferences = getApplicationContext().getSharedPreferences("uni.mse.mserss.INTERVAL", getApplicationContext().MODE_PRIVATE);
+        int index = preferences.getInt("interval", 0);
+        spinnerSettings.setSelection(index);
+    }
+
+    private void SaveSettings() {
+        preferences = getApplicationContext().getSharedPreferences("uni.mse.mserss.INTERVAL", getApplicationContext().MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("interval", spinnerSettings.getSelectedItemPosition());
+        editor.commit();
     }
 }
